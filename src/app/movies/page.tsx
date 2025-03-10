1cd01891fb26979a2127b7aa6da3b2b0ddb83cfc
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-11 01:12:42 +0530

    extend data

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -5,6 +5,23 @@ export default function MoviesPage() {
     { id: "1", title: "Inception" },
     { id: "2", title: "Interstellar" },
     { id: "3", title: "The Dark Knight" },
+    { id: "4", title: "Avatar" },
+    { id: "5", title: "Titanic" },
+    { id: "6", title: "The Matrix" },
+    { id: "7", title: "Pulp Fiction" },
+    { id: "8", title: "Forrest Gump" },
+    { id: "9", title: "The Godfather" },
+    { id: "10", title: "The Shawshank Redemption" },
+    { id: "11", title: "Fight Club" },
+    { id: "12", title: "The Lord of the Rings: The Return of the King" },
+    { id: "13", title: "The Lion King" },
+    { id: "14", title: "Gladiator" },
+    { id: "15", title: "Joker" },
+    { id: "16", title: "Whiplash" },
+    { id: "17", title: "Parasite" },
+    { id: "18", title: "The Silence of the Lambs" },
+    { id: "19", title: "Django Unchained" },
+    { id: "20", title: "The Green Mile" },
   ];
 
   return (
